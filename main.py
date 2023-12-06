06ec31fe861da3e67ef78ff6c5b1f422e9b072dd
Elegy <2218023235@qq.com>
2023-12-06 16:48:34 +0800

    websocket 实现流式传输

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # app.py
 from flask import Flask, request, jsonify, send_from_directory,Response,stream_with_context
+from flask_socketio import SocketIO, emit
+from flask_socketio import disconnect
+from test import conversation
 from generate_suject import PaperSubjectGenerator
 from follow import paper_md
 from login import  UserAuthenticator
@@ -8,8 +11,12 @@ paper_md=paper_md()
 paper_subject=PaperSubjectGenerator()
 
 app = Flask(__name__)
-
-
+# 创建 SocketIO 实例
+socketio = SocketIO()
+# 将 SocketIO 实例与 Flask 应用关联，并设置跨域请求的来源
+socketio.init_app(app, cors_allowed_origins='*')
+# 定义一个命名空间
+name_space = '/subject'
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -49,18 +56,43 @@ def typo():
     response, status = paper_md.check_typo(data)
     return jsonify(response), status
 
-@app.route('/api/subject', methods=['POST'])
-def subject():
-    data = request.json
-    response, status = paper_subject.generate_subject(data)
-    return jsonify(response), status
 
 @app.route('/essaywriter-head.svg')
 def serve_svg():
     return send_from_directory('static', 'essaywriter-head.svg')
 
 
+@app.route('/api/subject')
+def subject():
+    # response, status = paper_subject.generate_subject(data)
+    # 定义要广播的事件名称
+    event_name = 'echo'
+    # 定义要广播的数据
+    broadcasted_data = {'data': "test message!"}
+    # 使用 SocketIO 实例发送消息，但不进行广播（只发送给发起请求的客户端）
+    socketio.emit(event_name, broadcasted_data, broadcast=False, namespace=name_space)
+    return 'done!'
+
+# 处理 WebSocket 连接事件
+@socketio.on('connect', namespace=name_space)
+def connected_msg():
+    # 打印提示信息
+    print('client connected.')
+
+# 处理 WebSocket 断开连接事件
+@socketio.on('disconnect', namespace=name_space)
+def disconnect_msg():
+    # 打印提示信息
+    print('client disconnected.')
+
+# 处理来自客户端的特定事件（my_event）
+@socketio.on('my_event', namespace=name_space)
+def test_message(message):
+    conversation(message['data'])
+    disconnect(sid=request.sid)
+
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
 
--- test.py
+++ test.py
@@ -1,32 +1,48 @@
 import os
 from langchain.chat_models import ChatOpenAI
-from langchain.callbacks.base import BaseCallbackManager,BaseCallbackHandler
-from langchain.schema import HumanMessage, SystemMessage
-
+from langchain.callbacks.base import BaseCallbackManager, BaseCallbackHandler
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from flask_socketio import  emit
+# 环境变量设置
 os.environ["OPENAI_API_KEY"] = "sk-83oW8ytjMFgzbSCY2a90Ee8355974d33B1289f7eA657882e"
 os.environ["OPENAI_API_BASE"] = "https://api.xbbdkj.com/v1"
+
+
 class StreamOnlyCallbackHandler(BaseCallbackHandler):
     def on_llm_new_token(self, token: str, **kwargs: any) -> any:
-        print("")
-        # return token
+        emit('my_response', {'token': token})
+
+
 # 初始化 ChatOpenAI 对象
 chat = ChatOpenAI(
-    model_name="gpt-4-1106-preview",
-    # gpt-3.5-turbo,
+    model_name="gpt-3.5-turbo",
     streaming=True,
     verbose=True,
-    callback_manager=BaseCallbackManager([StreamOnlyCallbackHandler()])
+    callback_manager=BaseCallbackManager([StreamOnlyCallbackHandler()]),
 )
 
-# 定义要发送给模型的消息
-messages = [
-    # SystemMessage(content='You are an AI friend.'),
-    HumanMessage(content="你是什么模型"),
-]
+# 读取文件内容并将其添加为系统消息
+with open('/home/ubuntu/llm_paper/prompt/subject.txt', 'r') as file:
+    file_contents = file.read()
 
-chat(messages)
+# Prompt
+prompt = ChatPromptTemplate(
+    messages=[
+        SystemMessagePromptTemplate.from_template(file_contents),
+        # The `variable_name` here is what must align with memory
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{answer}"),
+    ]
+)
 
-# 调用模型并直接打印到控制台
-for response in chat(messages):
-    # print(111)
-    print(response)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+conversation = LLMChain(llm=chat, prompt=prompt, verbose=True, memory=memory)
+
+
